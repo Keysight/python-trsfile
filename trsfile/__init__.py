b2fa061432f3ee89f770c3bc3479e1eb94d15e16
Stefan Droege <droege@riscure.com>
2022-01-24 10:39:12 +0100

    Add back the __all__ definition that I accidentaly removed earlier.

--- trsfile/__init__.py
+++ trsfile/__init__.py
@@ -11,23 +11,37 @@ from trsfile.engine.engine import Engine
 from trsfile.engine.trs import TrsEngine
 from trsfile.engine.file import FileEngine
 
-def trs_open(path, mode = 'r', **options):
-	"""Reads, modifies or creates a :py:obj:`TraceSet` with a specific storage
-	engine (defaults to :py:obj:`TrsEngine`).
+__all__ = [
+    'trs_open',
+    'Trace',
+    'TraceSet',
+    'SampleCoding',
+    'Header',
+    'TracePadding',
+    'Engine',
+    'TrsEngine',
+    'FileEngine',
+]
 
-	:param path: path to the file or directory
-	:param mode: mode how to open the file or directory (same as the default
-		Python :py:obj:`open`)
-	:param options: zero or more options that are passed down to the
-		:py:obj:`TraceSet` and the storage engine. Available options can be found
-		in the different storage engines. The storage engine can be selected with
-		:code:`engine = 'TrsEngine'` (default value).
-	:type path: str
-	:type mode: str
-	:type options: dict(str, any)
-	:returns: instance of a new or initialized :py:obj:`TraceSet`
-	:rtype: TraceSet
-	"""
-	return TraceSet(path, mode, **options)
+
+def trs_open(path, mode='r', **options):
+    """Reads, modifies or creates a :py:obj:`TraceSet` with a specific storage
+    engine (defaults to :py:obj:`TrsEngine`).
+
+    :param path: path to the file or directory
+    :param mode: mode how to open the file or directory (same as the default
+        Python :py:obj:`open`)
+    :param options: zero or more options that are passed down to the
+        :py:obj:`TraceSet` and the storage engine. Available options can be found
+        in the different storage engines. The storage engine can be selected with
+        :code:`engine = 'TrsEngine'` (default value).
+    :type path: str
+    :type mode: str
+    :type options: dict(str, any)
+    :returns: instance of a new or initialized :py:obj:`TraceSet`
+    :rtype: TraceSet
+    """
+    return TraceSet(path, mode, **options)
+
 
 open = trs_open
