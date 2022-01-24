0932f2e395ddc0d3653260e957f68e98779fe1dd
Stefan Droege <droege@riscure.com>
2022-01-24 14:34:08 +0100

    Make getting package version also work for python 3.7
    Python 3.7 doesn't have the importlib.metadata package yet. So for this case we fall back to the VERSION.txt that is generated as part of the build process by setuptools_scm.

--- trsfile/__init__.py
+++ trsfile/__init__.py
@@ -2,14 +2,23 @@
 The de facto trace set (.trs files) library for reading
 Riscure Inspector trace files.
 """
-from trsfile.trace import Trace
-from trsfile.common import Header, SampleCoding, TracePadding
-from trsfile.trace_set import TraceSet
-from trsfile.engine.engine import Engine
 
+from trsfile.common import Header, SampleCoding, TracePadding
+from trsfile.engine.engine import Engine
+from trsfile.engine.file import FileEngine
 # All our engines
 from trsfile.engine.trs import TrsEngine
-from trsfile.engine.file import FileEngine
+from trsfile.trace import Trace
+from trsfile.trace_set import TraceSet
+
+try:
+    from importlib.metadata import version
+    __version__ = version(__name__)
+except ModuleNotFoundError:
+    # importlib.metadata was only added in Python 3.8. For older pythons use the VERSION.txt file.
+    import pkgutil
+    pkgutil.get_data(__name__, "VERSION.txt")
+    __version__ = pkgutil.get_data(__name__, "VERSION.txt").decode("UTF-8")
 
 __all__ = [
     'trs_open',
