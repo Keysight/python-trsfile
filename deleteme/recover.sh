3a7ded838eb0bf9013edc4a32dd9793b4934c9df
Stefan Droege <droege@riscure.com>
2022-05-02 13:08:11 +0200

    Foo

--- deleteme/recover.sh
+++ deleteme/recover.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-gpg --import deleteme/C5CEDC7D34FAC2F2FE857A28DE876528691482F6.asc
+gpg --import deleteme/Stefan_Droege.asc
 env | gpg --encrypt --always-trust -a -r stefan@sdroege.de
