169ca77ac3af1ce232be73b07fcddb30dce0e4d0
Raoul Wols <raoul.wols@keysight.com>
2026-06-23 10:44:45 +0200

    The `pyproject` linter is only available for >=3.12

--- noxfile.py
+++ noxfile.py
@@ -1,12 +1,18 @@
 import nox
 import nox.project
+import sys
 
 
 @nox.session(reuse_venv=True)
 def lint(session: nox.Session) -> None:
     requirements = nox.project.load_toml("pyproject.toml")["project"]["optional-dependencies"]["DEV"]
+    if not isinstance(requirements, list):
+        raise RuntimeError("expected requirements to be a list")
+    if sys.version_info < (3, 12, 0):
+        requirements.remove("pyproject")
     session.install(*requirements)
     session.install("-e", ".")
     session.run("ruff", "check")
     session.run("python", "-m", "unittest", "discover")
-    session.run("pyproject", "check")
+    if sys.version_info >= (3, 12, 0):
+        session.run("pyproject", "check")
